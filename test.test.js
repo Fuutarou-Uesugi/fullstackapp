117da45d38f379c825f548c3887c04e1303d6a47
fimovol <fimovolpe@gmail.com>
2022-07-04 20:28:00 -0500

    more

--- test.test.js
+++ test.test.js
@@ -1,12 +1,14 @@
 import {suma} from './components/suma'
 
-test('adds 1 + 2 to equal 3', () => {
-    expect(suma(1, 2)).toBe(3);
-});
-
-test('adds 1 + 4 to equal 5', () => {
-    expect(suma(1, 4)).toBe(5);
-});
-test('adds 0 + 0 to equal 0', () => {
-    expect(suma(0, 0)).toBe(0);
-});
+describe('hola jeje',()=> {
+    test('adds 1 + 2 to equal 3', () => {
+        expect(suma(1, 2)).toBe(3);
+    });
+    
+    test('adds 1 + 4 to equal 5', () => {
+        expect(suma(1, 4)).toBe(5);
+    });
+    test('adds 0 + 0 to equal 0', () => {
+        expect(suma(0, 0)).toBe(0);
+    });
+})
